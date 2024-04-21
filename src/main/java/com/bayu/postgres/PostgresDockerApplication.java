6eced2d7b2ba360854df13ead2b6a113bf20e920
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-21 11:24:06 +0700

    Initial commit

---
+++ src/main/java/com/bayu/postgres/PostgresDockerApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/postgres/PostgresDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
