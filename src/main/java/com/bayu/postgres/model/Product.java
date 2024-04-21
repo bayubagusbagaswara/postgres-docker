e58da23e3f67f55d9eb47c9a376b012e310bcb59
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-21 15:16:07 +0700

    add model product

---
+++ src/main/java/com/bayu/postgres/model/Product.java
@@ -0,0 +1,29 @@
+package com.bayu.postgres.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+}
